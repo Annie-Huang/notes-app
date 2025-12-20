8eeec379661118d65cfc1b17a77148a4a9546e27
Annie Huang <annie.hn@gmail.com>
2025-12-20 15:33:48 +1100

    Change index.jsx to Home component and change export default XX to be the bottom

--- app/index.jsx
+++ app/index.jsx
@@ -1,10 +1,8 @@
 import { Text, View, StyleSheet } from "react-native";
 
-export default function Index() {
+const HomeScreen = () => {
   return (
-    <View
-      style={styles.container}
-    >
+    <View style={styles.container}>
       <Text>Edit app/index.tsx to edit this screen.</Text>
     </View>
   );
@@ -17,3 +15,5 @@ const styles = StyleSheet.create({
     alignItems: "center",
   }
 })
+
+export default HomeScreen;
