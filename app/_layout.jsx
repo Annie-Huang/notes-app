95274e24fce3a8719ce3b3fe79804196b47a5a1e
Annie Huang <annie.hn@gmail.com>
2026-01-04 20:39:22 +1100

    Wrapped the template within the root layout page (app/_layout.jsx) with <AuthProvider>

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,7 +1,8 @@
 import { Stack } from 'expo-router';
+import { AuthProvider } from '../contexts/AuthContext';
 
 const RootLayout = () => {
-  return (
+  /*  return (
     <Stack
       screenOptions={{
         headerStyle: { backgroundColor: '#ff8c00' },
@@ -15,9 +16,30 @@ const RootLayout = () => {
       }}
     >
       <Stack.Screen name='index' options={{ title: 'Home' }} />
-      {/* if you don't use headerTitle, the title will take name of the folder app/notes -> 'notes' */}
+      {/!* if you don't use headerTitle, the title will take name of the folder app/notes -> 'notes' *!/}
       <Stack.Screen name='notes' options={{ headerTitle: 'Notes' }} />
     </Stack>
+  );*/
+
+  return (
+    <AuthProvider>
+      <Stack
+        screenOptions={{
+          headerStyle: { backgroundColor: '#ff8c00' },
+          headerTintColor: '#fff',
+          headerTitleStyle: { fontSize: 20, fontWeight: 'bold' },
+          contentStyle: {
+            paddingHorizontal: 10,
+            paddingTop: 10,
+            backgroundColor: '#fff',
+          },
+        }}
+      >
+        <Stack.Screen name='index' options={{ title: 'Home' }} />
+        {/* if you don't use headerTitle, the title will take name of the folder app/notes -> 'notes' */}
+        <Stack.Screen name='notes' options={{ headerTitle: 'Notes' }} />
+      </Stack>
+    </AuthProvider>
   );
 };
 
