14ee70403740983160520af2f2af0ea1df875a01
Annie Huang <annie.hn@gmail.com>
2026-01-04 17:16:53 +1100

    Add all the state and ref into NoteItem.jsx

--- components/NoteItem.jsx
+++ components/NoteItem.jsx
@@ -1,6 +1,11 @@
+import { useState, useRef } from 'react';
 import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 const NoteItem = ({ note, onDelete }) => {
+  const [isEditing, setIsEditing] = useState(false);
+  const [editedText, setEditedText] = useState(note.text);
+  const inputRef = useRef(null);
+
   return (
     <View style={styles.noteItem}>
       <Text style={styles.noteText}>{note.text}</Text>
