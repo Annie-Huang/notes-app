fe46a779eda7d4d6999d6a60c9d6d8aee8b546cb
Annie Huang <annie.hn@gmail.com>
2026-01-04 21:17:43 +1100

    Create all the local state in app/auth/index.jsx

--- app/auth/index.jsx
+++ app/auth/index.jsx
@@ -1,6 +1,13 @@
 import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
 
 const AuthScreen = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setComfirmPassword] = useState('');
+  const [isRegistering, setIsRegistering] = useState(false);
+  const [error, setError] = useState(false);
+
   return (
     <View style={styles.container}>
       <Text>Auth</Text>
