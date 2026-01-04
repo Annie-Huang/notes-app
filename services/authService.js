ee9ce683a6c57bf39d5e61a982a6d7cec177da21
Annie Huang <annie.hn@gmail.com>
2026-01-04 19:51:36 +1100

    Create register function in services/authService.js to call account in appwrite to create user

---
+++ services/authService.js
@@ -0,0 +1,14 @@
+import { account } from './appwrite';
+import { ID } from 'react-native-appwrite';
+
+const authService = {
+  // Register a user
+  async register(email, password) {
+    try {
+      const response = await account.create(ID.unique(), email, password);
+      return response;
+    } catch (error) {}
+  },
+};
+
+export default authService;
