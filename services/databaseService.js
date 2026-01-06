59791cd0bb4a1f811284ba9c6a364664d62db313
Annie Huang <annie.hn@gmail.com>
2026-01-06 20:25:03 +1100

    Pass the queries to database.listDocuments call in databaseService.js

--- services/databaseService.js
+++ services/databaseService.js
@@ -2,9 +2,18 @@ import { database } from './appwrite';
 
 const databaseService = {
   // List Documents
-  async listDocuments(dbId, colId) {
+  // async listDocuments(dbId, colId) {
+  //   try {
+  //     const response = await database.listDocuments(dbId, colId);
+  //     return response.documents || [];
+  //   } catch (error) {
+  //     console.error('Error fetching documents:', error.message);
+  //     return { error: error.message };
+  //   }
+  // },
+  async listDocuments(dbId, colId, queries = []) {
     try {
-      const response = await database.listDocuments(dbId, colId);
+      const response = await database.listDocuments(dbId, colId, queries);
       return { data: response.documents || [], error: null };
     } catch (error) {
       console.error('Error fetching documents:', error.message);
